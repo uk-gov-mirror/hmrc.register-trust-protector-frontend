0208f8a893fa27f271fb5c853cd7f2270d590e59
karishmacharlwood-hmrc <295779981+karishmacharlwood-hmrc@users.noreply.github.com>
2026-07-17 14:54:47 +0100

    DDCE-8579 register-trust-protector-frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
+  val playBootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % playBootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % playBootstrapVersion,
     "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.19.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % Test)
